eb357b11f8a583269022993eddb2411ada15827a
DimaBorek <drackon4ik@gmail.com>
2018-06-01 21:51:09 +0300

    /prinInfo

--- src/com/company/LockWithCondition.java
+++ src/com/company/LockWithCondition.java
@@ -1,4 +1,10 @@
 package com.company;
 
 public class LockWithCondition {
+    public static void main(String[] args) {
+
+    }
+    public static void printInfo(){
+        System.out.println("Info");
+    }
 }
